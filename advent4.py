1b2d6c9bc7a93c5e7787c93cf9e1298c4f83f3c4
jlageman <78727276+jlageman@users.noreply.github.com>
2023-12-04 13:05:17 +0100

    Add files via upload

--- advent4.py
+++ advent4.py
@@ -16,18 +16,14 @@ for line in dat:
     l.append(points)
 answer1 = sum(l)
 
-cards = 0
+import math
 copies = [1] * len(dat)
 for y in range(len(dat)):
     for _ in range(copies[y]):
-        cards += 1
-        winning = dat[y].split(' | ')[0].split(': ')[1].split(' ')
-        winning = [x for x in winning if len(x)>0]
-        have = dat[y].split(' | ')[1].split(' ')
-        wins = 0
-        for num in have:
-            if num in winning:
-                wins += 1
+        if l[y] < 2:
+            wins = l[y]
+        else:
+            wins = int(math.log2(l[y])+1)
         for z in range(1,wins+1):
             copies[y+z] += 1
-answer2 = cards
+answer2 = sum(copies)
