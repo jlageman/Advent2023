4f647e679100bba61a9c5cb9ba2b4206a3538bf7
jlageman <78727276+jlageman@users.noreply.github.com>
2023-12-18 17:37:41 +0100

    Add files via upload

--- advent17.py
+++ advent17.py
@@ -54,10 +54,12 @@ while search:
     dx, dy = direction
     for ndx, ndy in ((-dy, dx), (dy, -dx)):
         new_d = d
-        for steps in range(5,11):
+        for steps in range(1,11):
             new_x, new_y = x + ndx*steps, y + ndy*steps
             if 0 <= new_x <= max(maps.keys())[0] and 0 <= new_y <= max(maps.keys())[1]:
                 new_d += maps[(new_x, new_y)]
+                if steps<4:
+                    continue
                 if new_d < dist[(new_x, new_y), (ndx, ndy)]:
                     dist[(new_x, new_y), (ndx, ndy)] = new_d
                     search.append([new_d, (new_x, new_y), (ndx, ndy)])
